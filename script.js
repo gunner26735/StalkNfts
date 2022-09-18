133e6d7dab73d67649a4c86d4a9519e118f959f6
gunner26735 <61881540+gunner26735@users.noreply.github.com>
2022-09-18 19:03:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 const APIKEY = 'ckey_bcf2a8cd82204dc3a01733fa007';
 const baseURL = 'https://api.covalenthq.com/v1';
-const blockchainChainId = '1'
+var blockchainChainId;
 var demoAddress =" ";
 //= '0xDc35C75d027E4E65824cC656f655BcA90505C722'// IMP addr : 0xe4605d46fd0b3f8329d936a8b258d69276cba264
 const token_ids = []
@@ -28,10 +28,13 @@ async function getNftData(chainId, address, token_id) {
 function getaddr(){
 	var demoAddress= document.getElementById("waddress").value;
 	console.log("The wallet address is  " + demoAddress);
-  console.log("I am executed")
-  console.log(typeof(demoAddress))
-getTokenData(blockchainChainId, demoAddress);
-  
+  	console.log("I am executed")
+  	console.log(typeof(demoAddress))
+	
+	var r_chainid = document.getElementById("format").value;
+	blockchainChainId = r_chainid
+	getTokenData(blockchainChainId, demoAddress);
+	console.log(r_chainid)
   }
 
 async function getTokenData(chainId, address) {
@@ -49,24 +52,20 @@ async function getTokenData(chainId, address) {
 	
 }
 
-
-
 function pushData(name_l, desc, image, price) {
+	if(price == null){
+		price = 0.0;
+	}
 	output += `
-			<div class="product">
-			<img src="${image}" alt="${desc}">
-				<p class="name">${name_l}</p>
-				<p class="description">${desc}</p>
-				<p class="price">
-					<span>${price}</span>
-					<span>&euro;</span>
-				</p>
-				<p class="cart">Add to cart <i class="bx bx-cart-alt"></i></p>
-			</div>
+	<div class="grid__item">
+		<div class="card"><img class="card__img" src="https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=2250&amp;q=80" alt="NFT IMAGE">
+		<div class="card__content">
+			<h1 class="card__header">${name_l}</h1>
+			<p class="card__text">${desc} </p>
+			<button class="card__btn">${price}<span>&rarr;</span></button>
+		</div>
+		</div>
+	</div>
 		`;
-		document.querySelector(".products").innerHTML = output;
+		document.querySelector(".grid").innerHTML = output;
 } 
-
-
-
-// Example address request
