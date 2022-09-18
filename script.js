5ce93979fe0eaa2fbca78480c04e51b0ec4b82b0
Shubham Patel <shubhamashishpatel@gmail.com>
2022-09-18 19:24:37 +0530

    Updated The script for dynamic data

--- script.js
+++ script.js
@@ -6,36 +6,49 @@ var demoAddress =" ";
 //= '0xDc35C75d027E4E65824cC656f655BcA90505C722'// IMP addr : 0xe4605d46fd0b3f8329d936a8b258d69276cba264
 const token_ids = []
 let output = "";
-async function getNftData(chainId, address, token_id) {
+async function getNftData(chainId, address) {
 
-	console.log("Address = "+address)
-	const url = new URL(`${baseURL}/${chainId}/tokens/${address}/nft_metadata/${token_id}/?key=${APIKEY}`);
+
+	const url = new URL(`${baseURL}/${chainId}/address/${address}/balances_v2/?key=${APIKEY}&nft=true`);
+	console.log("Url = " + url)
+
+	//const url = "https://api.covalenthq.com/v1/137/address/0xDc35C75d027E4E65824cC656f655BcA90505C722/balances_v2/?key=ckey_bcf2a8cd82204dc3a01733fa007&nft=true"
 
 	const response = await fetch(url);
 	const result = await response.json();
-	const data = result.data.items[0].nft_data[0];
+	const data = result.data.items;
+	for (var i = 0; i < data.length; i++) {
+		if (data[i]["nft_data"] != null) {
+			for (var j = 0; j < data[i]["nft_data"].length; j++) {
+				console.log("Nft = " + data[i]["nft_data"][j].external_data["name"])
+				const name_l = data[i]["nft_data"][j].external_data["name"];
+				const description_l = data[i]["nft_data"][j].external_data["description"];
+				const image_l = data[i]["nft_data"][j].external_data["image"];
+				const price = data[i]["nft_data"][j].token_quote_rate_eth
+				console.log("Nft Name = " + name_l + "Description =" + description_l + "image=" + image_l)
+
+				pushData(name_l, description_l, image_l, price);
+
+
+			}
+		}
+	}
 	console.log("Data = ", data)
-	const tokenid = data.token_id;
-	const name_l = data["external_data"].name;
-	const description_l = data["external_data"].description;
-	const image_l = data["external_data"].image;
-	//console.log("Nft Name = " + name_l + "Description =" + description_l + "image=" + image_l)
-	const price = data["token_quote_rate_eth"]
-	//console.log("Price = " + price)
-	pushData(name_l, description_l, image_l, price);
-	return data;
-} 
-function getaddr(){
-	var demoAddress= document.getElementById("waddress").value;
+	/*  */return data;
+}
+
+
+
+function getaddr() {
+	var demoAddress = document.getElementById("waddress").value;
 	console.log("The wallet address is  " + demoAddress);
-  	console.log("I am executed")
-  	console.log(typeof(demoAddress))
-	
-	var r_chainid = document.getElementById("format").value;
-	blockchainChainId = r_chainid
-	getTokenData(blockchainChainId, demoAddress);
-	console.log(r_chainid)
-  }
+	console.log("I am executed")
+	console.log(typeof (demoAddress))
+	getNftData(137, demoAddress);
+
+	//getTokenData(blockchainChainId, demoAddress);
+
+}
 
 async function getTokenData(chainId, address) {
 	const url = new URL(`${baseURL}/${chainId}/tokens/${address}/nft_token_ids/?key=${APIKEY}`);
@@ -58,7 +71,8 @@ function pushData(name_l, desc, image, price) {
 	}
 	output += `
 	<div class="grid__item">
-		<div class="card"><img class="card__img" src="https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=2250&amp;q=80" alt="NFT IMAGE">
+	
+		<div class="card"><img class="card__img" src=${image}" alt="NFT IMAGE">
 		<div class="card__content">
 			<h1 class="card__header">${name_l}</h1>
 			<p class="card__text">${desc} </p>
